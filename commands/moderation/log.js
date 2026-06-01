dc068d82ed5cab9f1462f46d28f0dfa731f4d927
Hero of Lylat <technical@bulbagarden.net>
2026-06-01 13:14:40 -0400

    Reformat to try...catch and remove unused sequelize wrapper

--- commands/moderation/log.js
+++ commands/moderation/log.js
@@ -3,7 +3,6 @@
  * Log a warning for a user.
  */
 import { EmbedBuilder, MessageFlags, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
-import sequelize from "../../includes/database/database.js";
 import config from "../../config.js";
 import { ModLogs } from "../../includes/database/index.js";
 
@@ -17,37 +16,36 @@ export const data = new SlashCommandBuilder()
   .addStringOption((reason) =>
     reason.setName("reason").setDescription("Reason for warning.").setRequired(true)
   );
+
 export async function execute(interaction) {
   const user = interaction.options.getUser("user");
   const reason = interaction.options.getString("reason");
 
-  // Log the actual warning
-  const results = await sequelize
-    .transaction(() => {
-      return ModLogs.create({
-        loggedID: user.id,
-        loggerID: interaction.user.id,
-        logName: "warning",
-        message: reason,
-      });
-    })
-    .catch((err) => {
-      // Error. Log it and tell the mod it failed.
-      console.log(err);
-      return interaction.reply({
-        content: `There was an error logging to database:\n${err}\nPlease inform the bot author.`,
-        flags: MessageFlags.Ephemeral,
-      });
+  try {
+    // Log the actual warning
+    const results = await ModLogs.create({
+      loggedID: user.id,
+      loggerID: interaction.user.id,
+      logName: "warning",
+      message: reason,
     });
 
-  // Success. Send the response.
-  const response = new EmbedBuilder()
-    .setColor(config.messageColors.memWarn)
-    .setTitle(`Warning logged for ${user.username}`)
-    .setDescription(
-      `Warning ID #${results.dataValues.id}\n` + `Logged by ${interaction.user.username}`
-    )
-    .addFields({ name: "Warning", value: reason }, { name: "ID", value: user.id })
-    .setTimestamp();
-  interaction.reply({ embeds: [response] });
+    // Success. Send the response.
+    const response = new EmbedBuilder()
+      .setColor(config.messageColors.memWarn)
+      .setTitle(`Warning logged for ${user.username}`)
+      .setDescription(
+        `Warning ID #${results.dataValues.id}\n` + `Logged by ${interaction.user.username}`
+      )
+      .addFields({ name: "Warning", value: reason }, { name: "ID", value: user.id })
+      .setTimestamp();
+    interaction.reply({ embeds: [response] });
+  } catch (err) {
+    // Something went wrong. Log it.
+    console.log(err);
+    return interaction.reply({
+      content: `There was an error logging to database.\nPlease inform the bot author.`,
+      flags: MessageFlags.Ephemeral,
+    });
+  }
 }
